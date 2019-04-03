848cf80e304e5bfbe2189b17c6eab84f24d47212
seanblue <5884067+seanblue@users.noreply.github.com>
2019-04-02 23:07:37 -0400

    Add option to see the overdue review count in the UI

--- prioritize-overdue-reviews.js
+++ prioritize-overdue-reviews.js
@@ -3,7 +3,7 @@
 // @namespace     https://www.wanikani.com
 // @description   Prioritize review items that are more overdue based on their SRS level and when the review became available.
 // @author        seanblue
-// @version       0.9.9
+// @version       0.9.10
 // @include       https://www.wanikani.com/review/session
 // @grant         none
 // ==/UserScript==
@@ -15,9 +15,15 @@
 	const shouldSortOverdueItemsKey = 'shouldSortOverdueItems';
 	const overdueThresholdPercentKey = 'overdueThresholdPercent';
 	const percentRandomItemsToIncludeKey = 'percentRandomItemsToInclude';
+	const shouldDisplayOverdueItemCountKey = 'shouldDisplayOverdueItemCount';
 
 	function promise(){var a,b,c=new Promise(function(d,e){a=d;b=e;});c.resolve=a;c.reject=b;return c;}
 	let settingsLoadedPromise = promise();
+
+	let overdueCountSpanId = 'por-overdue-count';
+	let overdueIconId = 'por-overdue-icon';
+	let originalOverdueReviewSet;
+	let alreadySetUpOverdueItemCountRendering = false;
 
 	// Prevent other scripts from hijacking Math.random by using a local version.
 	let localRandom = window.Math.random;
@@ -35,6 +41,7 @@
 	wkof.include('ItemData, Settings, Menu');
 	wkof.ready('document, Settings, Menu').then(loadSettings);
 	wkof.ready('document, ItemData').then(reorderReviews);
+	wkof.ready('document').then(setupUI);
 
 	function loadSettings() {
 		wkof.Menu.insert_script_link({ name: settingsScriptId, submenu:'Settings', title: settingsTitle, on_click: openSettings });
@@ -43,6 +50,7 @@
 		defaultSettings[overdueThresholdPercentKey] = 20;
 		defaultSettings[percentRandomItemsToIncludeKey] = 25;
 		defaultSettings[shouldSortOverdueItemsKey] = 'random';
+		defaultSettings[shouldDisplayOverdueItemCountKey] = false;
 
 		wkof.Settings.load(settingsScriptId, defaultSettings).then(function() {
 			settingsLoadedPromise.resolve();
@@ -56,15 +64,21 @@
 		settings[overdueThresholdPercentKey] = { type: 'number', label: 'Overdue Threshold (%)', hover_tip: 'When should a review be considered overdue? This is based on the SRS level and time since the review became available.&#013;WARNING: Setting this too low could harm your long term retention!' };
 		settings[percentRandomItemsToIncludeKey] = { type: 'number', label: 'Randomness Factor (%)', hover_tip: 'What percentage of the overdue queue should be filled with random items? Including random items helps prevent you from knowing too much about what reviews will show up.&#013;WARNING: Setting this too low could harm your long term retention!' };
 		settings[shouldSortOverdueItemsKey] = { type: 'dropdown', label: 'Overdue Item Sorting', content: {'random': 'Random','sorted':'Sorted'}, hover_tip: 'Should the overdue queue remain random or be sorted to prioritize the most overdue items?&#013;WARNING: Setting this to "Sorted" could harm your long term retention!' };
+		settings[shouldDisplayOverdueItemCountKey] = { type: 'checkbox', label: 'Display Overdue Count', hover_tip: 'Should the number of overdue items be displayed?' };
 
 		let settingsDialog = new wkof.Settings({
 			script_id: settingsScriptId,
 			title: settingsTitle,
-			on_save: reorderReviews,
+			on_save: onUpdateSettings,
 			settings: settings
 		});
 
 		settingsDialog.open();
+	}
+
+	function onUpdateSettings() {
+		setupUI();
+		reorderReviews();
 	}
 
 	function reorderReviews() {
@@ -143,10 +157,11 @@
 
 		window.overduePercentDictionary = overduePercentDictionary;
 
-		let reviewQueue = $.jStorage.get('activeQueue').concat($.jStorage.get('reviewQueue'));
+		let reviewQueue = getFullReviewQueue();
 		shuffle(reviewQueue); // Need to reshuffle in case the queue has already been sorted.
 
-		let overdueQueue = reviewQueue.filter(item => overduePercentDictionary[item.id] >= overdueThreshold);
+		originalOverdueReviewSet = getoriginalOverdueReviewSet(overduePercentDictionary, overdueThreshold);
+		let overdueQueue = reviewQueue.filter(item => originalOverdueReviewSet.has(item.id));
 		let notOverdueQueue = reviewQueue.filter(item => !overdueQueue.includes(item));
 
 		if (shouldSortOverdueItems) {
@@ -160,6 +175,17 @@
 		window.queue = queue;
 
 		updateQueueState(queue);
+	}
+
+	function getFullReviewQueue() {
+		return $.jStorage.get('activeQueue').concat($.jStorage.get('reviewQueue'));
+	}
+
+	function getoriginalOverdueReviewSet(overduePercentDictionary, overdueThreshold) {
+		let itemIds = Object.keys(overduePercentDictionary).map(key => parseInt(key));
+		let overdueItems = itemIds.filter(key => overduePercentDictionary[key] >= overdueThreshold);
+
+		return new Set(overdueItems);
 	}
 
 	// Fisher–Yates Shuffle
@@ -215,6 +241,36 @@
 		$.jStorage.set('activeQueue', activeQueue);
 		$.jStorage.set('reviewQueue', inactiveQueue);
 		$.jStorage.set('currentItem', activeQueue[0])
+	}
+
+	function setupUI() {
+		settingsLoadedPromise.then(function() {
+			let shouldSetUpOverdueItemCountRendering = wkof.settings[settingsScriptId][shouldDisplayOverdueItemCountKey];
+
+			if (shouldSetUpOverdueItemCountRendering && !alreadySetUpOverdueItemCountRendering) {
+				let overdueReviewCountSpan = $(`<span id="${overdueCountSpanId}">`);
+				$('#stats').prepend(overdueReviewCountSpan).prepend(`<i id="${overdueIconId}" class="icon-medkit" />`)
+
+				$.jStorage.listenKeyChange('currentItem', updateOverdueCountOnPage);
+
+				alreadySetUpOverdueItemCountRendering = true;
+			}
+			else if (!shouldSetUpOverdueItemCountRendering && alreadySetUpOverdueItemCountRendering) {
+				$(`#${overdueCountSpanId}`).remove();
+				$(`#${overdueIconId}`).remove();
+
+				$.jStorage.stopListening('currentItem', updateOverdueCountOnPage);
+
+				alreadySetUpOverdueItemCountRendering = false;
+			}
+		});
+	}
+
+	function updateOverdueCountOnPage(key) {
+		let remainingReviewIds = getFullReviewQueue().map(item => item.id);
+		let remainingOverdueReviewSet = new Set(remainingReviewIds.filter(id => originalOverdueReviewSet.has(id)));
+
+		$(`#${overdueCountSpanId}`).text(remainingOverdueReviewSet.size);
 	}
 
 })(window.jQuery, window.wkof);
